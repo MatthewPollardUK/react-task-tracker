e815e39bfa173354ce075a6479cfd2ccdbcfc031
MatthewPollardUK <matthewpoll@live.com>
2022-08-10 15:52:44 +0100

    front end UI complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -30,6 +32,13 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id ))
 }
 
+//Add Task
+const addTask = (task) => {
+const id = Math.floor(Math.random() * 10000) + 1
+const newTask = { id, ...task }
+setTasks([...tasks, newTask])
+}
+
 //Toggle Reminder
 const toggleReminder = (id) => {
   setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
@@ -37,7 +46,8 @@ const toggleReminder = (id) => {
 
   return (
   <div className="container">
-    <Header />
+    <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+  {showAddTask && <AddTask onAdd={addTask} />}
     {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> ) : ( 'No Task to show' )}
   </div>
   )
